a23459845469b479906ea22a888efe70b1ccd19c
Sam1234456 <148829817+Sam1234456@users.noreply.github.com>
2024-02-23 23:18:02 -0500

    Devoir 1 : Kawo

--- main.dart
+++ main.dart
@@ -1,214 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
 }
 
-class Member { // definit un modele de membre avec des proprietes
-  final String name;
-  final String position;
-  final String Tel;
-  final String bio;
-  final String academicBackground; // Ajout de la propriété academicBackground
-
-  Member({
-    required this.name,
-    required this.position,
-    required this.Tel,
-    required this.bio,
-    required this.academicBackground, // Initialisation de la propriété academicBackground
-  });
-}
-
-List<Member> members = [
-  Member(
-    name: 'Louis Stanley',
-    position: 'Développeur mobile',
-    Tel: '3111-4111',
-    bio: 'Louis Stanley est un développeur mobile passionné ...',
-    academicBackground: 'Baccalauréat en informatique, Université ESIH', // Exemple d'antécédents académiques
-  ),
-    Member(
-    name: 'Ovide Samuel',
-    position: 'Développeur mobile',
-    Tel: '4050-2090',
-    bio: 'Ovide Samuel est un développeur mobile passionné ...',
-    academicBackground: 'Baccalauréat en informatique, Université ESIH', // Exemple d'antécédents académiques
-  ),
-    Member(
-    name: 'Jean Exard Abellard',
-    position: 'Développeur mobile',
-    Tel: '3414-8784',
-    bio: 'Exard Abellard est un développeur mobile passionné ...',
-    academicBackground: 'Baccalauréat en informatique, Université ESIH', // Exemple d'antécédents académiques
-  ),
-    Member(
-    name: 'Steeve Emmanuel',
-    position: 'Développeur mobile',
-    Tel: '3234-5678',
-    bio: 'Steeve Emmanuel est un développeur mobile passionné ...',
-    academicBackground: 'Baccalauréat en informatique, Université ESIH', // Exemple d'antécédents académiques
-  ),
-  
-  // Ajoutez d'autres membres ici
-];
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GroupMemberList(),
-    );
-  }
-}
-
-class GroupMemberList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(' CV Membres du Groupe'),
-      ),
-      body: ListView.builder(
-        itemCount: members.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(members[index].name),
-            subtitle: Text(members[index].position),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MemberDetail(member: members[index]),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MemberDetail extends StatelessWidget {
-  final Member member;
-
-  MemberDetail({required this.member});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(member.name),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Personal Information'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PersonalInformation(member: member),
-                ),
-              );
-            },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Liste de Kawo'),
+        ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // Trois colonnes
           ),
-          ListTile(
-            leading: Icon(Icons.school),
-            title: Text('Academic Background'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => AcademicBackground(member: member),
+          itemBuilder: (BuildContext context, int index) {
+            // Personnalisation des kawos avec des couleurs aléatoires
+            return Card(
+              color: _randomColor(), // Couleur aléatoire pour chaque kawo
+              child: InkWell(
+                onTap: () {
+                  // Action à effectuer lorsqu'un kawo est cliqué
+                  print('Vous avez cliqué sur le kawo numéro $index');
+                },
+                child: Center(
+                  child: ListTile(
+                    title: Text(' ===$index==='),
+                    subtitle: Text('Karo $index'),
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PersonalInformation extends StatelessWidget {
-  final Member member;
-
-  PersonalInformation({required this.member});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Information'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Name: ${member.name}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Position: ${member.position}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Bio: ${member.bio}',
-              style: TextStyle(fontSize: 18),
-            ),
-            // Ajoutez d'autres informations personnelles ici
-          ],
+              ),
+            );
+          },
         ),
       ),
     );
   }
-}
 
-class AcademicBackground extends StatelessWidget {
-  final Member member;
-
-  AcademicBackground({required this.member});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Academic Background'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Academic Background:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              member.academicBackground, // Afficher les antécédents académiques du membre
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
+  // Fonction pour générer une couleur aléatoire
+  Color _randomColor() {
+    return Color.fromRGBO(
+      // Utilisation de valeurs aléatoires pour les composantes de couleur
+      // pour obtenir une couleur variée pour chaque kawo
+      _random.nextInt(256),
+      _random.nextInt(256),
+      _random.nextInt(256),
+      1.0, // Opacité fixée à 1.0
     );
   }
+
+  final _random = Random();
 }
-
-
-
-
-
-
-
-
-
-
